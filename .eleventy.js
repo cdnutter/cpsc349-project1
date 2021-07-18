aad0fe43e42d1899d36f756cd922e90b1b833891
Chris Nutter <cdnutter@gmail.com>
2021-07-17 20:10:06 -0700

    pushed using push.sh - princess peach

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
 };
+module.exports = {
+	pathPrefix: "/cpsc349-project1"
+	// Thanks to Group 5 for the help
+};
