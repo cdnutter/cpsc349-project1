eb27d1232ce96dbac8a4151dfd47991371cdfe2a
scottpark44 <scottpark44@csu.fullerton.edu>
2021-07-17 18:12:11 -0700

    Github_Pages_Implementation_2
    Testing again with script sections of json.

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,11 @@
-
-
-module.exports = function(eleventyConfig) {
-    eleventyConfig.setUseGitIgnore(false);
-};
-
 module.exports = {
     dir: {
         input: "src",
         output: "docs"
     }
+};
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.setUseGitIgnore(false);
+    
 };
