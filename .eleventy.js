9fc39a38d8e384aa2e12d4d1fd7530a885f3fd0d
Chris Nutter <cdnutter@gmail.com>
2021-07-17 20:27:08 -0700

    live 2

--- .eleventy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.setUseGitIgnore(false);
-};
-module.exports = {
-	pathPrefix: "/cpsc349-project1"
-	// Thanks to Group 5 for the help
-};
